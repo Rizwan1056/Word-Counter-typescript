4226d43a48c744ad6920b2fbf9d0e4eef59f2d22
Rizwan1056 <rizwi1056@gmail.com>
2024-06-28 00:44:28 +0500

    finally Done

---
+++ Index.ts
@@ -0,0 +1,16 @@
+#! /usr/bin/env node
+import inquirer from "inquirer"
+
+const answers:{
+    sentence: string 
+} = await inquirer.prompt([
+    {
+        name: "sentence",
+        type:"input",
+        message:"Enter your sentence to count the words:"
+    }
+])
+
+const words = answers.sentence.trim().split(" ")
+console.log(words)
+console.log(`Your sentence word count is ${words.length}`);
